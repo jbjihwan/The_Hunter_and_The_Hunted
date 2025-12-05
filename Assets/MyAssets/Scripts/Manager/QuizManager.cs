34de8e54bf47c0c68ba2e144d77e83a1922af3a6
ShinYongJun <4rest528@naver.com>
2025-12-05 16:34:25 +0900

    곱셈왕 완성

--- Assets/MyAssets/Scripts/Manager/QuizManager.cs
+++ Assets/MyAssets/Scripts/Manager/QuizManager.cs
@@ -1,23 +1,73 @@
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
 
 public class QuizManager : MonoBehaviour
 {
     [System.Serializable]
     public class Quiz
     {
+        public string quiz;
         public string[] options;
         public int answerIndex;
     }
 
+    public static QuizManager Instance;
     public Quiz[] quizzes;
+    public TextMeshProUGUI quizText;
+
+    private int quizIndex;
+    private Queue<string> quizQueue;
+    private GameObject quizUI;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
-
+        Helper.Shuffle(quizzes);
+        quizIndex = 0;
+        quizQueue = new Queue<string>();
+        quizUI = quizText.transform.parent.gameObject;
     }
 
-    void Update()
+    public void UpdateQuizUI()
     {
+        if (quizQueue.Count > 0)
+        {
+            quizUI.SetActive(true);
+            quizText.text = quizQueue.Peek();
+        }
+        else
+        {
+            quizUI.SetActive(false);
+        }
+    }
 
+    public Quiz GetQuiz()
+    {
+        Quiz quiz = quizzes[quizIndex];
+        quizIndex = (quizIndex + 1) % quizzes.Length;
+
+        quizQueue.Enqueue(quiz.quiz);
+        UpdateQuizUI();
+
+        return quiz;
+    }
+
+    public void PopQuiz()
+    {
+        quizQueue.Dequeue();
+        UpdateQuizUI();
     }
 }
